4f70de4fe51061a7657c26a2fffcc43abaa28e9f
zheny <eugeniapareeva@yandex.ru>
2023-02-26 11:49:47 +0300

    site

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-
 const swiper = new Swiper('.swiper', {
-  /* autoplay: {
+  autoplay: {
     delay: 4000,
     disableOnInteraction: false
-  }, */
+  },
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   }
 });
+
 const menuMobileBtn = document.querySelector('.menu-mobile__btn');
 const menuMobileList = document.querySelector('.menu-mobile__list');
-
 menuMobileBtn.addEventListener('click', () => {
   menuMobileList.classList.toggle('menu--open');
 });
